8f7a8c66daf7eb346ff9659a5c6188e8529a3289
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-29 20:56:24 +0100

    8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A script that that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """Return an empty list if no document in the collection"""
+    docs = list(mongo_collection.find())
+    return docs
