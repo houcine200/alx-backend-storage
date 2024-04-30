6a68f71c26f4f4a844b5c201782339c6bda2da61
Houcine Walaq <houcine.walaq@gmai.com>
2024-04-30 18:47:09 +0100

    12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Provides some stats about Nginx logs stored in MongoDB"""
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client.logs
+collection = db.nginx
+
+total_logs = collection.count_documents({})
+print("{} logs".format(total_logs))
+
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    count = collection.count_documents({"method": method})
+    print("\tmethod {}: {}".format(method, count))
+
+count_get_status = collection.count_documents({"method": "GET", "path": "/status"})
+print("{} status check".format(count_get_status))
